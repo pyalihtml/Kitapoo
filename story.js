963ab79ec9ae5fd471bc14d58551e19931ebb271
Yuji Itadori <bakareren09@gmail.com>
2022-09-22 00:05:44 +0300

    Update story.js

--- story.js
+++ story.js
@@ -4,23 +4,33 @@ function Alessandro(){
         imageUrl: 'https://i.pinimg.com/736x/f5/46/81/f546813de22aab16e159ef4c393818ae--mafia-party-mobsters.jpg',
         imageWidth: 250,
         imageHeight: 250,
+        width: '700px',
         imageAlt: 'Alessandro',
         text: 'Tam atlayacakken bir adam tuttu beni. Ne olduğunu anlayamamıştım. Beyaz tenli, kahverengi saçlı, uzun boylu bir adamdı... ',
         confirmButtonText: 'Hikayeye Git.',
-
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         showCancelButton: true,
         confirmButtonText: 'Hikayeye Git.',
 
-    }).then((result) => {
+    })
+    .then((result) => {
+
         if (result.isConfirmed) {
           Swal.fire(
             'Alessandro Lorenzo',
-            'Brooklyn köprüsüne çıktım, ayağıma taş bağladım ve atlamak için hazırlandım zor bir an değildi benim için. Bıkmıştım bu hayattan. Tam atlayacakken bir adam tuttu beni. Ne olduğunu anlayamamıştım. Beyaz tenli, kahverengi saçlı, uzun boylu bir adamdı. Kıyafeti tam bir mafya gibiydi. Siyah takımı ve bir şapkası vardı. Belinde duran silahı da mafya olduğunu kanıtlar nitelikteydi. İsmimi sordu, Alessandro dedim ve onun ismini sordum. Cevap vermedi ve onu takip etmemi söyledi...',
+            'Brooklyn köprüsüne çıktım, ayağıma taş bağladım ve atlamak için hazırlandım zor bir an değildi benim için. Bıkmıştım bu hayattan. Tam atlayacakken bir adam tuttu beni. Ne olduğunu anlayamamıştım. Beyaz tenli, kahverengi saçlı, uzun boylu bir adamdı. Kıyafeti tam bir mafya gibiydi. Siyah takımı ve bir şapkası vardı. Belinde duran silahı da mafya olduğunu kanıtlar nitelikteydi. İsmimi sordu, Alessandro dedim ve onun ismini sordum. Cevap vermedi ve onu takip etmemi söyledi...'
           )
         }
+    })
+}
 
-      
-      })
+
+function bridge(){
+  Swal.fire({
+    title: 'Uyarı',
+    icon: 'error',
+    text: 'Bu hikaye henüz yazılmadı.'
+  }
+  )
 }
